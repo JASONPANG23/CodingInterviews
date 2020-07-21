1cf524bdb6509ce9acb790489f569d66aace2a22
jason <jasonpang@qq.com>
2020-07-21 22:18:57 +0800

    style: 添加解题思路

--- src/top/jasonpang/solution/jz48_不用加减乘除做加法/Solution001.java
+++ src/top/jasonpang/solution/jz48_不用加减乘除做加法/Solution001.java
@@ -8,6 +8,9 @@ package top.jasonpang.solution.jz48_不用加减乘除做加法;
  */
 public class Solution001 {
     public int Add(int num1,int num2) {
+        //计算a和b的无进位和，和进位
+        //如果进位不为0，则说明a+b的结果等于无进位和+进位，此时，把无进位和作为a，进位作为b，继续计算
+        //如果进位等于0， 说明此时a+b的结果就等于无进位和，返回无进位和即可。
         while(num2 != 0){
             int c = (num1 & num2) << 1 ; // 进位
             num1 ^= num2 ; //不进位
