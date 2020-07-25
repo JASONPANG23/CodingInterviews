56c685adc908ed76fdb988c61fa2993fbc464be2
庞杰升 <pangjiesheng@lizhi.fm>
2020-07-25 10:22:57 +0800

    feat: jz48_不用加减乘除做加法

---
+++ src/top/jasonpang/solution/jz48_不用加减乘除做加法/Solution001.java
@@ -0,0 +1,14 @@
+package top.jasonpang.solution.jz48_不用加减乘除做加法;
+
+/**
+ * description:
+ *
+ * @author Jason
+ * Created in 2020-07-20 19:27.
+ */
+public class Solution001 {
+    public int Add(int num1,int num2) {
+        //todo
+        return -1;
+    }
+}
