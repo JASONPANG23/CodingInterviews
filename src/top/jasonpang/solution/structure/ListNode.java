48c2c6dce58230ec02c875e5642d1e126260b910
JasonPang23 <754109057@qq.com>
2020-07-25 13:01:40 +0800

    feat: 定义结构体

---
+++ src/top/jasonpang/solution/structure/ListNode.java
@@ -0,0 +1,15 @@
+package top.jasonpang.solution.structure;
+
+/**
+ * description:
+ * 链表节点
+ * @author Jason
+ * Created in 2020-07-25 12:58.
+ */
+public class ListNode {
+    int val ;
+    ListNode next ;
+    ListNode(int x){
+        val = x;
+    }
+}
---
+++ src/top/jasonpang/solution/structure/TreeNode.java
@@ -0,0 +1,16 @@
+package top.jasonpang.solution.structure;
+
+/**
+ * description:
+ *
+ * @author Jason
+ * Created in 2020-07-25 13:00.
+ */
+public class TreeNode {
+    int val ;
+    TreeNode left ;
+    TreeNode right;
+    TreeNode(int x){
+        val = x ;
+    }
+}
