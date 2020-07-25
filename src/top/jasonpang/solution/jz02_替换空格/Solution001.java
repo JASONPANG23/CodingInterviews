7e4eb322018cd813e7affea6dd52222f75bf5900
JasonPang23 <754109057@qq.com>
2020-07-25 12:55:14 +0800

    feat: jz02_替换空格

---
+++ src/top/jasonpang/solution/jz02_替换空格/Solution001.java
@@ -0,0 +1,14 @@
+package top.jasonpang.solution.jz02_替换空格;
+
+/**
+ * description:
+ *
+ * @author Jason
+ * Created in 2020-07-25 12:49.
+ */
+public class Solution001 {
+    public String replaceSpace(StringBuffer str) {
+        //todo
+        return null;
+    }
+}
