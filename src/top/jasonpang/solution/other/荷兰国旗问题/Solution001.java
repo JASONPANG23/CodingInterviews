14f5ccfb532c61d3ae882b9ff64b1682a5dc207c
JasonPang23 <754109057@qq.com>
2020-08-02 15:24:39 +0800

    feat: 荷兰国旗问题

--- src/top/jasonpang/solution/other/荷兰国旗问题/Solution001.java
+++ src/top/jasonpang/solution/other/荷兰国旗问题/Solution001.java
@@ -10,9 +10,40 @@ package top.jasonpang.solution.other.荷兰国旗问题;
 public class Solution001 {
 
 
-    private static void swap(int[] arr,int a,int b){
-        arr[a] = arr[a] ^ arr[b];
-        arr[b] = arr[a] ^ arr[b];
-        arr[a] = arr[a] ^ arr[b];
+    public void function(int [] arr,int num){
+        //[0...less]    小于num
+        //(less,cur)    等于num
+        //[cur...length]  大于num
+        int less = - 1 ;
+        int more = arr.length ;
+        int cur = 0;
+        while(cur < more){
+            if(arr[cur] < num){
+                swap(arr,cur++,++less);
+            }
+            else if(arr[cur] > num){
+                swap(arr,cur,--more);
+            }else{
+                cur ++;
+            }
+        }
+        System.out.println("小于区域 :[0..."+less+"]");
+        System.out.println("等于区域 :("+less+"..."+cur+")");
+        System.out.println("大于区域 :["+cur+"..."+arr.length+"]");
+    }
+
+    private void swap(int[] arr,int a,int b){
+        int temp = arr[a] ;
+        arr[a] = arr[b]  ;
+        arr[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1,545,3,5,6,765,87,5,78,46,4};
+        int num = 4;
+        new Solution001().function(arr,num);
+        for (int value : arr) {
+            System.out.print(value+" ");
+        }
     }
 }
