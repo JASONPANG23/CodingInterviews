a209471c9b9955fb775c9d9a267c4a4291ff2afa
JasonPang23 <754109057@qq.com>
2020-08-08 10:05:46 +0800

    jz27_字符串的排列_solution002

--- src/top/jasonpang/solution/jz27_字符串的排列/Solution001.java
+++ src/top/jasonpang/solution/jz27_字符串的排列/Solution001.java
@@ -6,6 +6,8 @@ import java.util.*;
  * description:
  * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。
  * 例如输入字符串abc,则按字典序打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+ *
+ * todo: 动态规划实现
  * @author Jason
  * Created in 2020-08-05 23:17.
  */
